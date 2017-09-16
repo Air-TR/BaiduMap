52f5e453ff478af43bd2134e272db985935e9553
Air-TR <tr1838@163.com>
2017-09-16 11:58:24 +0800

    1

--- src/main/java/cn/tr/baidumap/test/Combination.java
+++ src/main/java/cn/tr/baidumap/test/Combination.java
@@ -1,7 +1,7 @@
 package cn.tr.baidumap.test;
 
-import java.util.ArrayList;  
-import java.util.List;  
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 
@@ -12,35 +12,35 @@ import java.util.List;
  *
  */
 public class Combination {
-	
-	public static void main(String args[]){  
-        char chs[]={'a','b','c'};  
-        combiantion(chs);  
-    }
-	
-    public static void combiantion(char chs[]){  
-        if(chs==null||chs.length==0){  
-            return ;  
-        }  
-        List<Character> list=new ArrayList<Character>();  
-        for(int i=1;i<=chs.length;i++){  
-            combine(chs,0,i,list);  
-        }  
-    }  
-    
-    // 从字符数组中第begin个字符开始挑选number个字符加入list中  
-    public static void combine(char []cs,int begin,int number,List<Character> list){  
-        if(number==0){  
-            System.out.println(list.toString());  
-            return ;  
-        }  
-        if(begin==cs.length){  
-            return;  
-        }  
-        list.add(cs[begin]);  
-        combine(cs,begin+1,number-1,list);  
-        list.remove((Character)cs[begin]);  
-        combine(cs,begin+1,number,list);  
-    }  
-    
+
+	public static void main(String args[]) {
+		char chs[] = { 'a', 'b', 'c' };
+		combiantion(chs);
+	}
+
+	public static void combiantion(char chs[]) {
+		if (chs == null || chs.length == 0) {
+			return;
+		}
+		List<Character> list = new ArrayList<Character>();
+		for (int i = 1; i <= chs.length; i++) {
+			combine(chs, 0, i, list);
+		}
+	}
+
+	// 从字符数组中第begin个字符开始挑选number个字符加入list中
+	public static void combine(char[] cs, int begin, int number, List<Character> list) {
+		if (number == 0) {
+			System.out.println(list.toString());
+			return;
+		}
+		if (begin == cs.length) {
+			return;
+		}
+		list.add(cs[begin]);
+		combine(cs, begin + 1, number - 1, list);
+		list.remove((Character) cs[begin]);
+		combine(cs, begin + 1, number, list);
+	}
+
 }
